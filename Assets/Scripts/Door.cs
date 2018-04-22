74dbb775747b21efeaecd544239f010986ee6c39
Kayne Ruse <kayneruse@gmail.com>
2018-04-22 22:06:39 +1000

    Door-switch puzzles

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : MonoBehaviour {
+	SpriteRenderer spriteRenderer;
+	BoxCollider2D boxCollider;
+
+	public PressurePlate trigger;
+	public bool inverted = false;
+
+	void Awake() {
+		spriteRenderer = GetComponent<SpriteRenderer> ();
+		boxCollider = GetComponent<BoxCollider2D> ();
+	}
+
+	void Update() {
+		if (trigger.pressed) {
+			spriteRenderer.enabled = inverted;
+			boxCollider.enabled = inverted;
+		} else {
+			spriteRenderer.enabled = !inverted;
+			boxCollider.enabled = !inverted;
+		}
+	}
+}
